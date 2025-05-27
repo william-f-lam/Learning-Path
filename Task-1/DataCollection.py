15116719a6b6dd0c3eafa08036e4b93035518e18
william-f-lam <williamfugilam@gmail.com>
2025-05-27 14:57:31 -0700

    Update DataCollection.py

--- Task-1/DataCollection.py
+++ Task-1/DataCollection.py
@@ -5,6 +5,8 @@ import Task1Credentials
 def main():
     with sync_playwright() as p:
         station_ids = []
+        base_url = "https://pems.dot.ca.gov/?dnode=VDS&content=loops&tab=det_timeseries&station_id="
+        truck_flow_results = []
 
         # Open PeMS website.
         try:
@@ -43,10 +45,22 @@ def main():
                     station_ids.append(text)
         except:
             print("Failed to store all station IDs.")
-        finally:
-            browser.close()
+        
+        # For each station, check for and store truck flow data.
+        for index, station in enumerate(station_ids):
+            try:
+                page.goto(base_url + str(station))
+                page.wait_for_selector("select[id=q]")
+                page.select_option("select[id=q]", value = "truck_flow", timeout = 500)
+                truck_flow_results.append({"ID" :station, "Flow" : 1})
+            except:
+                print("Truck flow not available at station: " + station + ".")
+                truck_flow_results.append({"ID" :station, "Flow" : 0})
+        browser.close()
+        print(truck_flow_results)
+        df_truck_flow_results = pd.DataFrame(truck_flow_results)
+        df_truck_flow_results.to_csv("Truck_Flow_Results.csv", index = False)
+        
 
 if __name__ == "__main__":
     main()
-
-    
