626a47e9e417460d7dea2eab889063a9cf661d9d
william-f-lam <williamfugilam@gmail.com>
2025-05-27 02:08:25 -0700

    Update DataCollection.py

--- Task-1/DataCollection.py
+++ Task-1/DataCollection.py
@@ -4,6 +4,8 @@ import Task1Credentials
 
 def main():
     with sync_playwright() as p:
+        station_ids = []
+
         # Open PeMS website.
         try:
             browser = p.chromium.launch(headless = False, slow_mo = 50)
@@ -20,17 +22,31 @@ def main():
         except:
             print("Log-in Failed")
 
-        # Collect station IDs.
-        try:
-            # Enter the report finder for I-5N, District 7, Stations dataset.
+        # Enter the report finder for I-5N, District 7, Stations dataset.
+        try: 
             page.select_option("select#rptFinderFwy_fwy", value = "5")
             page.select_option("select#rptFinderFwy_location", value = "7")
             page.select_option("select#rptFinderFwy_report_type", label = "Stations")
-
-            # View all IDs and store into a dataframe for later use.
-
         except:
-            print("Failed to gather station IDs.")
+            print("Failed to view station IDs.")
+        
+        # Store all station IDs for I5-N, District 7, Stations.
+        try:
+            page.wait_for_selector("th:has-text('ID')")
+            page.goto(page.url + "&pagenum_all=1")
+
+            # Credit to ChatGPT for helping find the station IDs.
+            links = page.locator("a[href*='station_id=']")
+            for i in range(links.count()):
+                text = links.nth(i).inner_text().strip()
+                if text.isdigit():
+                    station_ids.append(text)
+        except:
+            print("Failed to store view all station IDs.")
+        finally:
+            browser.close()
 
 if __name__ == "__main__":
     main()
+
+    
